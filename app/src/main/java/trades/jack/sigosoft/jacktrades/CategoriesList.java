0410e048d60903725343ff89491555730b805cff
praveenwebqua <sequoia16.32petaflops>
2017-02-07 08:44:06 +0530

    Login, Registration done

---
+++ app/src/main/java/trades/jack/sigosoft/jacktrades/CategoriesList.java
@@ -0,0 +1,13 @@
+package trades.jack.sigosoft.jacktrades;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CategoriesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories_list);
+    }
+}
---
+++ app/src/main/java/trades/jack/sigosoft/jacktrades/ProductPage.java
@@ -0,0 +1,13 @@
+package trades.jack.sigosoft.jacktrades;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ProductPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_page);
+    }
+}
